c0202c7f0f6e99eaa6319ebb2cfbb1615d04f190
nurullah91 <mdnurullah.ptk@gmail.com>
2023-04-10 23:46:29 +0600

    available job section added

--- src/Components/JobCard/JobCard.jsx
+++ src/Components/JobCard/JobCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const JobCard = () => {
+const JobCard = ({job}) => {
+    const {img, category, jobs} = job;
     return (
-        <div>
-            job card
+        <div className='bg-slate-100 p-7 rounded-md'>
+           <img src={img} alt="icon" />
+           <h3 className='text-xl font-bold my-2'>{category}</h3>
+           <p className='text-gray-500'>{jobs} jobs available</p>
         </div>
     );
 };
--- src/Components/JobCategory/JobCategory.jsx
+++ src/Components/JobCategory/JobCategory.jsx
@@ -1,38 +1,56 @@
 import React from 'react';
+import JobCard from '../JobCard/JobCard'
 
 const JobCategory = () => {
-    const jobCategories = [
+
+
+  const jobCategories = [
+    {
+      id: 1,
+      img: "chip-1.png",
+      category: "Software Engineering",
+      jobs: 200
+    },
+    {
+      id: 2,
+      img: "accounts-1.png",
+      category: "Data Science",
+      jobs: 300
+    },
+    {
+      id: 3,
+      img: "social-media-1.png",
+      category: "Digital Marketing",
+      jobs: 150
+    },
+    {
+      id: 4,
+      img: "business-1.png",
+      category: "Graphic Design",
+      jobs: 100
+    }
+  ];
+
+  return (
+    <div className='w-11/12 mx-auto lg:w-9/12'>
+      <div className='text-center'>
+        <h2 className='font-bold text-3xl my-2'>Job Category List</h2>
+        <p className='mb-5'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+      </div>
+
+
+      <div className='grid lg:grid-cols-4 md:grid-cols-2 gap-3'>
         {
-          id: 1,
-          image: "https://example.com/image1.jpg",
-          category: "Software Engineering",
-          availableJobs: 200
-        },
-        {
-          id: 2,
-          image: "https://example.com/image2.jpg",
-          category: "Data Science",
-          availableJobs: 300
-        },
-        {
-          id: 3,
-          image: "https://example.com/image3.jpg",
-          category: "Digital Marketing",
-          availableJobs: 150
-        },
-        {
-          id: 4,
-          image: "https://example.com/image4.jpg",
-          category: "Graphic Design",
-          availableJobs: 100
+          jobCategories.map(job => <JobCard
+          key={job.id}
+          job={job}
+          ></JobCard>)
         }
-      ];
-      
-    return (
-        <div>
-            job JobCategory
-        </div>
-    );
+
+      </div>
+
+    </div>
+  );
 };
 
 export default JobCategory;
