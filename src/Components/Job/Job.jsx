6a05a1c5dbea944c71f7233e3220b83cdcf01b1f
nurullah91 <mdnurullah.ptk@gmail.com>
2023-04-11 17:37:43 +0600

    job card design complete

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const Job = () => {
+
+
+
+
+const Job = ({ job }) => {
+    const { company_logo, job_title, company, job_type, duty_time, location, salary } = job;
     return (
-        <div>
-            Job Components
+        <div className='border rounded-md p-7'>
+            <img src={company_logo} alt="company logo" />
+            <h3 className='text-2xl font-bold my-3'>{job_title}</h3>
+            <p className='text-xl font-bold text-gray-500 mb-5'>{company}</p>
+            <button className='btn-outline mr-4'>{job_type}</button>
+            <button className='btn-outline'>{duty_time}</button>
+
+            <div className="flex font-bold text-gray-500 tracking-wide my-5">
+                <div className='flex  mr-8'>
+                    <img src="Frame-4.png" alt="location-icon" />
+                    <span>{location}</span>
+                </div>
+               
+                    <h3 className='flex'><img src="Frame.png" alt="dollar-icon" /> Salary:  
+                        <span>{salary}k</span></h3>
+
+              
+            </div>
+            <button className='btn-primary'>View Details</button>
+
+
         </div>
     );
 };
--- src/Components/Jobs/Jobs.jsx
+++ src/Components/Jobs/Jobs.jsx
@@ -13,18 +13,18 @@ console.log(jobs);
 
 
     return (
-        <div className='text-center'>
-            <h1 className='text-3xl font-bold'>Featured jobs</h1>
-            <p className='mb-8'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+        <div className='w-11/12 lg:w-9/12 mx-auto'>
+            <h1 className='text-3xl font-bold text-center'>Featured jobs</h1>
+            <p className='mb-8 text-center'>Explore thousands of job opportunities with all the information you need. Its your future</p>
 
-            <div className='grid md:grid-cols-2 gap-3'>
+            <div className='grid md:grid-cols-2 gap-5'>
                 {
                    jobs.slice(0, !showAll ? 4:jobs.length).map(job => <Job key={job.id} job= {job}></Job>)
                 }
             </div>
-            <div>
+            <div className='text-center'>
                {
-                 !showAll && <button onClick={() => setShowAll(true)} className='btn-primary'>See all jobs</button>
+                 !showAll && <button onClick={() => setShowAll(true)} className='btn-primary m-10'>See all jobs</button>
                }
             </div>
 
